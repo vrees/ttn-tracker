c50a32c5e718b4596691839e7d4fd3d3a6981140
vrees <viktor.rees@gmx.de>
2020-12-12 09:10:21 +0100

    .

--- flask_app/config.py
+++ flask_app/config.py
@@ -15,10 +15,7 @@ app_key = "key ttn-account-v2.t1A5JKlhe47sPKS596YtrvEzJ3UMwFtgoboTwaIqEYw"
 
 # Application devices
 devices = [
-    "ttgo-t-beam-esp32-ttnmapper",      // green housing
-    "ttgo-t-beam-esp32-ttnmapper-2",    // blue housing
-    "ttgo-t-beam-esp32-ttnmapper-3",
-    "ttgo-t-beam-esp32-ttnmapper-4"
+    "ttgo-t-beam-esp32-ttnmapper-3"
 ]
 
 # Where to place gateway markers
